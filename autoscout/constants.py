51e945bc265cbe32353e1189094cc14c2e5ca617
Dawid <dawid87872@gmail.com>
2025-04-12 00:32:38 +0200

    feat: define `URL` enum

--- autoscout/constants.py
+++ autoscout/constants.py
@@ -1,1 +1,12 @@
+import enum
+
 IMPLICIT_WAIT_TIME = 3
+
+
+class URL(enum.StrEnum):
+    """Application URLs."""
+
+    LOGIN = "https://info-car.pl/oauth2/login"
+    LOGIN_REDIRECT = "https://info-car.pl/new"
+    CHECK_FREE_DATES = "https://info-car.pl/new/prawo-jazdy/sprawdz-wolny-termin"
+    EXAM_DETAILS = "https://info-car.pl/new/prawo-jazdy/zapisz-sie-na-egzamin-na-prawo-jazdy/szczegoly-egzaminu"
