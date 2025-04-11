64dc66b99dfea09238eea6a4953b20f5363b7120
Dawid <dawid87872@gmail.com>
2025-04-12 00:40:59 +0200

    feat: define `ID` enum for DOM identifiers

--- autoscout/constants.py
+++ autoscout/constants.py
@@ -10,3 +10,11 @@ class URL(enum.StrEnum):
     LOGIN_REDIRECT = "https://info-car.pl/new"
     CHECK_FREE_DATES = "https://info-car.pl/new/prawo-jazdy/sprawdz-wolny-termin"
     EXAM_DETAILS = "https://info-car.pl/new/prawo-jazdy/zapisz-sie-na-egzamin-na-prawo-jazdy/szczegoly-egzaminu"
+
+
+class ID(enum.StrEnum):
+    """DOM IDs."""
+
+    LOGIN_EMAIL_INPUT = "username"
+    LOGIN_PASSWORD_INPUT = "password"
+    LOGIN_BUTTON = "register-button"
