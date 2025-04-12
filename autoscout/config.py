870612af213caa31a311d7f3e724a4b0d9d3bb86
Dawid <dawid87872@gmail.com>
2025-04-12 13:52:24 +0200

    feat: update `date` converter

--- autoscout/config.py
+++ autoscout/config.py
@@ -8,9 +8,7 @@ import typing
 
 DATE_FORMAT = "%Y-%m-%d"
 TYPE_CONVERTERS: dict[type, collections.abc.Callable[[str], typing.Any]] = {
-    datetime.date: lambda value: datetime.datetime.strptime(value, DATE_FORMAT)
-    .replace(tzinfo=datetime.UTC)
-    .date(),
+    datetime.date: lambda value: datetime.datetime.strptime(value, DATE_FORMAT).date(),
     bool: lambda value: value == "True",
     int: lambda value: int(value),
 }
