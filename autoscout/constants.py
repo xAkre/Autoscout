86aa116384314ce3a59cc460270953869a94440c
Dawid <dawid87872@gmail.com>
2025-04-12 13:52:03 +0200

    feat: define more `Selector` constants

--- autoscout/constants.py
+++ autoscout/constants.py
@@ -3,6 +3,7 @@ import enum
 from selenium.webdriver.common.by import By
 
 IMPLICIT_WAIT_TIME = 3
+_30_MINUTES = 30 * 60
 
 
 class URL(enum.StrEnum):
@@ -28,3 +29,15 @@ class Selector(enum.Enum):
     SUBMIT_BUTTON = (By.CSS_SELECTOR, 'button[type="submit"]')
     PRACTICE_EXAM_TYPE = (By.CSS_SELECTOR, 'input[aria-label="PRACTICE"]')
     REJECT_COOKIES_BUTTON = (By.ID, "cookiescript_reject")
+    EARLIEST_DATE_H5 = (By.CSS_SELECTOR, '.accordion-button[aria-expanded="true"] > h5')
+    EARLIEST_APPOINTMENT_BUTTON = (By.ID, "practiceExamsButton0")
+    CONFIRM_EARLIEST_APPOINTMENT_BUTTON = (By.ID, "confirm-modal-btn")
+    CREDENTIALS_FIRST_NAME_INPUT = (By.ID, "firstname")
+    CREDENTIALS_LAST_NAME_INPUT = (By.ID, "lastname")
+    CREDENTIALS_PESEL_INPUT = (By.ID, "pesel")
+    CREDENTIALS_PKK_INPUT = (By.ID, "pkk")
+    CREDENTIALS_EMAIL_INPUT = (By.ID, "email")
+    CREDENTIALS_PHONE_INPUT = (By.ID, "phoneNumber")
+    CREDENTIALS_ACCEPT_REGULATIONS_CHECKBOX = (By.ID, "regulations-text")
+    CREDENTIALS_NEXT_BUTTON = (By.ID, "next-btn")
+    CONFIRM_RESERVATION_BUTTON = (By.CSS_SELECTOR, 'nav .inavigation__btn--forward button[type="submit"].ghost-btn')
